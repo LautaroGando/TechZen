b0c5b4b60f6f7e3200b302e31b835fbdf45eaaa6
Lautaro Gando <lautarogandodev@gmail.com>
2024-10-16 17:20:19 -0300

    Proyecto Finalizado.

--- front/src/app/dashboard/Dashboard.tsx
+++ front/src/app/dashboard/Dashboard.tsx
@@ -9,7 +9,7 @@ export const Dashboard: React.FC = (): React.ReactElement => {
 
     return (
 
-        <div className="min-h-[calc(100vh-220px)] flex flex-col justify-center items-center gap-10 md:h-[calc(100vh-220px)] md:flex-row lg:flex-row">
+        <div className="min-h-[calc(100vh-220px)] flex flex-col justify-center items-center gap-10 md:min-h-[calc(100vh-220px)] md:flex-row lg:flex-row">
             <ImageUser />
             <div className="h-[1px] w-full lBColor md:w-[1px] md:h-[400px]"></div>
             <InfoUser />
