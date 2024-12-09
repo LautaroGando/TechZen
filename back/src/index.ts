3212dd1cb2437c52bb6921e83bd9c7bb459a74d7
Lautaro Gando <133940955+LautaroGando@users.noreply.github.com>
2024-12-09 16:40:00 -0300

    Update index.ts

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,20 +1,22 @@
 import { PORT } from "./config/envs";
 import app from "./server";
-import "reflect-metadata"
+import "reflect-metadata";
 import { AppDataSource } from "./config/dataSource";
 import { preLoadCategories } from "./helpers/preLoadCategories";
 import { preLoadProducts } from "./helpers/preLoadProducts";
+import pool from "./config/db";
 
 const initialize = async () => {
-    console.log("Initializing server");
-    await AppDataSource.initialize();
-    console.log("Database initialized");
-    await preLoadCategories();
-    await preLoadProducts();
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
+  console.log("Initializing server");
+  const dbTestResult = await pool.query("SELECT NOW()");
+  console.log("PostgreSQL connected:", dbTestResult.rows[0].now);
+  await AppDataSource.initialize();
+  console.log("Database initialized");
+  await preLoadCategories();
+  await preLoadProducts();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
 initialize();
-
